374d50d729f177a11e70728ef34491d0620de656
Masaaki Morishita <ikasama3.0@gmail.com>
2014-10-13 20:35:53 +0900

    Write test code (Tapestry::Test new)

--- lib/tapestry.rb
+++ lib/tapestry.rb
@@ -5,14 +5,14 @@ module Tapestry
   class Test
 
     def initialize(num_of_tests)
-      @num_of_tests = number_of_tests ||= 0
+      @num_of_tests = num_of_tests || 0
       plan
     end
 
     private
 
     def plan
-      p '1..' + @num_of_tests.to_s
+      print("1..#{@num_of_tests}\n")
     end
 
   end
--- spec/tapestry_spec.rb
+++ spec/tapestry_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
 
+def capture(stream)
+  begin
+    stream = stream.to_s
+    eval "$#{stream} = StringIO.new"
+    yield
+    result = eval("$#{stream}").string
+  ensure
+    eval("$#{stream} = #{stream.upcase}")
+  end
+  result
+end
+
+
 describe Tapestry do
+
   it 'has a version number' do
     expect(Tapestry::VERSION).not_to be nil
+  end
+
+  it 'Tapestry::Test.new' do
+    stdout = capture(:stdout) {
+      Tapestry::Test.new(5)
+    }
+    expect(stdout).to eq("1..5\n")
   end
 end
