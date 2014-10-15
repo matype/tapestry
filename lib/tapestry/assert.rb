6bdec0f0d794a705835e2478876ebbcb6bb7451b
Masaaki Morishita <ikasama3.0@gmail.com>
2014-10-15 22:28:54 +0900

    Inplement tapestry.dont_equal() method

--- lib/tapestry/assert.rb
+++ lib/tapestry/assert.rb
@@ -20,6 +20,27 @@ module Tapestry
     alias_method :is_equal_to, :equal
     alias_method :is_equal_in, :equal
 
+
+    def dont_equal(expected, actual, msg, test_num)
+      test_num = '' unless test_num
+
+      if expected != actual
+        print("ok #{test_num}\n")
+      else
+        print("not ok #{test_num}\n")
+      end
+
+      print("# #{msg}") if msg != ''
+
+      result(expected, actual) if expected == actual
+    end
+
+    alias_method :dosent_equal, :dont_equal
+    alias_method :isnt, :dont_equal
+    alias_method :isnt_equal_to, :dont_equal
+    alias_method :isnt_equal_in, :dont_equal
+
+
     private
 
     def result(expected, actual)
--- spec/tapestry_spec.rb
+++ spec/tapestry_spec.rb
@@ -29,16 +29,33 @@ describe Tapestry do
   it 'tapestry.equal (ok)' do
     tapestry = Tapestry::Test.new(1)
     stdout = capture(:stdout) {
-      tapestry.equal(1, 1, 'ok test', 1)
+      tapestry.equal(1, 1, 'tapestry.equal(): ok test', 1)
     }
-    expect(stdout).to eq("ok 1\n# ok test")
+    expect(stdout).to eq("ok 1\n# tapestry.equal(): ok test")
   end
 
   it 'tapestry.equal (not ok)' do
     tapestry = Tapestry::Test.new(1)
     stdout = capture(:stdout) {
-      tapestry.equal(1, 2, 'not ok test', 2)
+      tapestry.equal(1, 2, 'tapestry.equal(): not ok test', 2)
     }
-    expect(stdout).to eq("not ok 2\n# not ok test ---\n    actual:   2\n  expected:   1\n ---\n")
+    expect(stdout).to eq("not ok 2\n# tapestry.equal(): not ok test ---\n    actual:   2\n  expected:   1\n ---\n")
   end
+
+  it 'tapestry.dont_equal (ok)' do
+    tapestry = Tapestry::Test.new(1)
+    stdout = capture(:stdout) {
+      tapestry.dont_equal(1, 2, 'tapestry.dont_equal(): ok test', 3)
+    }
+    expect(stdout).to eq("ok 3\n# tapestry.dont_equal(): ok test")
+  end
+
+  it 'tapestry.dont_equal (not ok)' do
+    tapestry = Tapestry::Test.new(1)
+    stdout = capture(:stdout) {
+      tapestry.dont_equal(1, 1, 'tapestry.dont_equal(): not ok test', 4)
+    }
+    expect(stdout).to eq("not ok 4\n# tapestry.dont_equal(): not ok test ---\n    actual:   1\n  expected:   1\n ---\n")
+  end
+
 end
